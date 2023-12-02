945a176cd7cef75f11c84e4b98292379ae51779b
JGoo99 <zzgg3040@naver.com>
2023-12-03 02:35:06 +0900

    feat: db 구현

--- src/main/java/com/example/dividend/persist/entity/CompanyEntity.java
+++ src/main/java/com/example/dividend/persist/entity/CompanyEntity.java
@@ -1,5 +1,7 @@
 package com.example.dividend.persist.entity;
 
+import com.example.dividend.model.Company;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,14 +11,23 @@ import lombok.ToString;
 @Getter
 @ToString
 @NoArgsConstructor
+@Schema(description = "Company DB")
 public class CompanyEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(description = "회사 식별 아이디", example = "1")
   private Long companyId;
 
   @Column(unique = true)
+  @Schema(description = "회사 식별 코드", example = "COKE")
   private String ticker;
 
+  @Schema(description = "회사명", example = "Coca-Cola Consolidated, Inc.")
   private String name;
+
+  public CompanyEntity(Company company) {
+    this.ticker = company.getTicker();
+    this.name = company.getName();
+  }
 }
--- src/main/java/com/example/dividend/persist/entity/DividendEntity.java
+++ src/main/java/com/example/dividend/persist/entity/DividendEntity.java
@@ -1,5 +1,7 @@
 package com.example.dividend.persist.entity;
 
+import com.example.dividend.model.Dividend;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,16 +16,27 @@ import java.time.LocalDateTime;
 @Getter
 @ToString
 @NoArgsConstructor
+@Schema(description = "Dividend DB")
 public class DividendEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(description = "배당금 식별 아이디", example = "1")
   private Long dividendId;
 
+  @Schema(description = "회사 식별 아이디", example = "1")
   private Long companyId;
 
+  @Schema(description = "배당금 지급 시점", example = "2023-12-03T00:00:00")
   private LocalDateTime date;
 
+  @Schema(description = "배당금액", example = "0.25")
   private String dividend;
+
+  public DividendEntity(Long companyId, Dividend dividend) {
+    this.companyId = companyId;
+    this.date = dividend.getDate();
+    this.dividend = dividend.getDividend();
+  }
 }
 
