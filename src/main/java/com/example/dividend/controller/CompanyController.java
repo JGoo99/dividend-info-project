6a1b3ba977637bd97b5824a83fd32422864ae0e2
JGoo99 <zzgg3040@naver.com>
2023-12-03 03:11:08 +0900

    feat: 배당금 저장 구현
    - 회사 정보 저장 (company DB)
    - 회사 아이디와 매칭하여 배당금 리스트 저장 (dividend DB)

--- src/main/java/com/example/dividend/controller/CompanyController.java
+++ src/main/java/com/example/dividend/controller/CompanyController.java
@@ -1,11 +1,22 @@
 package com.example.dividend.controller;
 
+import com.example.dividend.model.Company;
+import com.example.dividend.service.CompanyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/company")
+@AllArgsConstructor
+@Tag(name = "Company", description = "Company API")
 public class CompanyController {
+
+  private final CompanyService companyService;
 
   @GetMapping("/autocomplete")
   public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
@@ -18,8 +29,17 @@ public class CompanyController {
   }
 
   @PostMapping
-  public ResponseEntity<?> addCompany() {
-    return null;
+  @Operation(summary = "add company data", description = "회사 정보를 저장한다.")
+  public ResponseEntity<?> addCompany(
+    @Schema(name = "회사정보", implementation = Company.class, example = "MMM")
+    @RequestBody Company request) {
+    String ticker = request.getTicker().trim();
+    if (ObjectUtils.isEmpty(ticker)) {
+      throw new RuntimeException("ticker is empty");
+    }
+
+    Company company = this.companyService.save(ticker);
+    return ResponseEntity.ok(company);
   }
 
   @DeleteMapping
---
+++ src/main/java/com/example/dividend/service/CompanyService.java
@@ -0,0 +1,51 @@
+package com.example.dividend.service;
+
+import com.example.dividend.model.Company;
+import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.persist.entity.CompanyEntity;
+import com.example.dividend.persist.entity.DividendEntity;
+import com.example.dividend.persist.repository.CompanyRepository;
+import com.example.dividend.persist.repository.DividendRepository;
+import com.example.dividend.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CompanyService {
+
+  public final Scraper yahooFinanceScraper;
+
+  private final CompanyRepository companyRepository;
+  private final DividendRepository dividendRepository;
+
+  public Company save(String ticker) {
+    boolean exists = this.companyRepository.existsByTicker(ticker);
+    if (exists) {
+      throw new RuntimeException("already exists ticker -> " + ticker);
+    }
+    return this.storeCompanyAndDividend(ticker);
+  }
+
+  private Company storeCompanyAndDividend(String ticker) {
+    Company company = this.yahooFinanceScraper.scrapCompanyByTicker(ticker);
+    if (ObjectUtils.isEmpty(company)) {
+      throw new RuntimeException("failed to scrap company by ticker -> " + ticker);
+    }
+
+    ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(company);
+
+    CompanyEntity companyEntity = this.companyRepository.save(new CompanyEntity(company));
+    List<DividendEntity> dividendEntities =
+      scrapedResult.getDividends().stream()
+        .map(e -> new DividendEntity(companyEntity.getCompanyId(), e))
+        .toList();
+
+    this.dividendRepository.saveAll(dividendEntities);
+
+    return company;
+  }
+}
