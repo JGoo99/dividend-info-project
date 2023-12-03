9f92574ebf8fce871b99b44344c7170ee84f1a32
JGoo99 <zzgg3040@naver.com>
2023-12-03 11:45:17 +0900

    feat: 배당금 정보 조회 기능 구현

--- src/main/java/com/example/dividend/controller/FinanceController.java
+++ src/main/java/com/example/dividend/controller/FinanceController.java
@@ -1,5 +1,7 @@
 package com.example.dividend.controller;
 
+import com.example.dividend.service.FinanceService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/finance")
+@AllArgsConstructor
 public class FinanceController {
+
+  private final FinanceService financeService;
 
   @GetMapping("/dividend/{companyName}")
   public ResponseEntity<?> searchFinance(@PathVariable String companyName) {
-    return null;
+
+    var dividend = this.financeService.getDividendByCompanyName(companyName);
+    return ResponseEntity.ok(dividend);
   }
 
 }
