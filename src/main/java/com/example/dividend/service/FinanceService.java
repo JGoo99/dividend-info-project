f0bc91759ecc5246b2fcc3cfb32fd2d4e5817f4b
JGoo99 <zzgg3040@naver.com>
2023-12-04 14:43:40 +0900

    feat: redis cache 기능 추가

--- src/main/java/com/example/dividend/service/FinanceService.java
+++ src/main/java/com/example/dividend/service/FinanceService.java
@@ -3,11 +3,13 @@ package com.example.dividend.service;
 import com.example.dividend.model.Company;
 import com.example.dividend.model.Dividend;
 import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.model.constants.CacheKey;
 import com.example.dividend.persist.entity.CompanyEntity;
 import com.example.dividend.persist.entity.DividendEntity;
 import com.example.dividend.persist.repository.CompanyRepository;
 import com.example.dividend.persist.repository.DividendRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,6 +22,7 @@ public class FinanceService {
   private final CompanyRepository companyRepository;
   private final DividendRepository dividendRepository;
 
+  @Cacheable(key = "#companyName", value = CacheKey.KEY_FINANCE)
   public ScrapedResult getDividendByCompanyName(String companyName) {
     CompanyEntity company = companyRepository.findByName(companyName)
       .orElseThrow(() ->
@@ -30,19 +33,10 @@ public class FinanceService {
 
     List<Dividend> dividends = new ArrayList<>();
     for (var entity : dividendEntities) {
-      dividends.add(Dividend.builder()
-        .date(entity.getDate())
-        .dividend(entity.getDividend())
-        .build());
+      dividends.add(new Dividend(entity.getDate(), entity.getDividend()));
     }
 
     return new ScrapedResult(
-      Company.builder()
-        .ticker(company.getTicker())
-        .name(company.getName())
-        .build(),
-
-      dividends
-      );
+      new Company(company.getTicker(), company.getName()), dividends);
   }
 }
